554352c0940a814a51575eea981c699626c8005c
tamc <tamc@greenonblack.com>
2010-02-28 19:27:31 +0000

    First commit

---
+++ lib/ruby_script_writer.rb
@@ -0,0 +1,64 @@
+class RubyScriptWriter
+  attr_reader :r
+  
+  def initialize
+    @r = StringIO.new
+    @tab_count = 0
+  end
+  
+  def indent
+    @tab_count += 1
+  end
+  
+  def outdent
+    @tab_count -= 1
+  end
+  
+  def put_class(class_name,superclass_name = nil)
+    puts "class ", class_name, superclass_name && " < ", superclass_name
+    indent
+    yield self
+    outdent
+    puts "end"
+    puts
+  end
+  
+  def put_simple_method(method_name,*method_code)
+    puts "def ", method_name,"; ",*method_code.join,"; end"
+  end
+  
+  def put_method(method_name,*arguments)
+    if arguments.empty?
+      puts "def ", method_name
+    else
+      puts "def ", method_name, "(", arguments.join(','), ")"
+    end
+    indent
+    yield self
+    outdent
+    puts "end"
+    puts
+  end
+  
+  def comment(*args)
+    puts "# ", *args
+  end
+  
+  def puts(*args)
+    if args.empty?
+      r.puts
+      return self
+    end
+    args.compact!
+    r.puts tabs + args.join unless args.empty?
+    self
+  end
+  
+  def tabs
+    "  " * @tab_count
+  end
+
+  def to_s
+    r.string
+  end
+end
---
+++ spec/ruby_script_writer_spec.rb
@@ -0,0 +1,93 @@
+$:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
+require 'ruby_script_writer'
+
+describe RubyScriptWriter do
+  
+  it "#initialize should create an empty string by default" do
+    r = RubyScriptWriter.new
+    r.to_s.should == ""
+  end
+  
+  it "#puts should allow any text to be put, with newlines added automatically" do
+    r = RubyScriptWriter.new
+    r.puts "# Hello world"
+    r.puts "# again"
+    r.to_s.should == "# Hello world\n# again\n"
+  end
+  
+  it "#puts doesn't put anything if a nil value is supplied" do
+    r = RubyScriptWriter.new
+    r.puts nil
+    r.to_s.should == ""
+  end
+  
+  it "#puts puts a new line if no argument is supplied" do
+    r = RubyScriptWriter.new
+    r.puts
+    r.to_s.should == "\n"
+  end
+  
+  it "#puts should handle several arguments, concatenating them onto the same line" do
+    r = RubyScriptWriter.new
+    r.puts "one", " ", 2, 3.0
+    r.to_s.should == "one 23.0\n"
+  end
+  
+  it "#indent should add two spaces to the start a line for every time indent is called" do
+    r = RubyScriptWriter.new
+    r.puts "no indent"
+    r.indent
+    r.puts "one indent"
+    r.indent
+    r.puts "two indents"
+    r.to_s.should == "no indent\n  one indent\n    two indents\n"
+  end
+  
+  it "#outdent should reverse the effect of an indent every time outdent is called" do
+    r = RubyScriptWriter.new
+    r.puts "no indent"
+    r.indent
+    r.outdent
+    r.puts "still no indent"
+    r.indent
+    r.indent
+    r.outdent
+    r.puts "one indent"
+    r.to_s.should == "no indent\nstill no indent\n  one indent\n"
+  end
+  
+  it "#comment adds a # to the start of the given line" do
+    r = RubyScriptWriter.new
+    r.comment "This is a comment"
+    r.to_s.should == "# This is a comment\n"
+  end
+  
+  it "#put_class generates classes, optionally with a superclass" do
+    r = RubyScriptWriter.new
+    r.put_class "One" do
+      r.comment "part of class One"
+    end
+    r.put_class "Two", "One" do
+      r.comment "part of class Two"
+    end
+    r.to_s.should == "class One\n  # part of class One\nend\n\nclass Two < One\n  # part of class Two\nend\n\n"
+  end
+  
+  it "#put_simple_method generates single line methods" do
+    r = RubyScriptWriter.new
+    r.put_simple_method "a23", "b23 + 10"
+    r.to_s.should == "def a23; b23 + 10; end\n"
+  end
+  
+  it "#put_method for generating multiple line methods, optionally with arguments" do
+    r = RubyScriptWriter.new
+    r.put_method "one" do 
+      r.comment "part of method one"
+    end
+    r.put_method "two","a","b","c" do
+      r.comment "part of method two"
+    end
+    r.to_s.should == "def one\n  # part of method one\nend\n\ndef two(a,b,c)\n  # part of method two\nend\n\n"
+  end
+  
+end
